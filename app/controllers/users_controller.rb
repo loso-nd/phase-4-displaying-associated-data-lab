c7d8d8678a728cb6a1284ac4dc5a5e30c1100c10
Nahum <losond8@gmail.com>
2021-07-29 07:26:46 -0700

    commit tree d2de7fcd96790529ad3f0f5bc573949ff7c3ff35

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,6 @@
 class UsersController < ApplicationController
+    def show 
+        user = User.find_by(id: params[:id])
+        render json: user, include: :items, except: [:created_at, :updated_at]
+    end
 end
